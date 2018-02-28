1feae10c857aa1035851c32564d34b217d9e3d2a
Jupath <attila.taligas@gmail.com>
2018-02-28 10:33:52 +0100

    Add private routes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,30 +1,15 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// // import ReactShallowRenderer from 'react-test-renderer/shallow';
-// import Header from '../../componenets/Header';
-
-// // test('should render Header correctly', () => {
-// //   const renderer = new ReactShallowRenderer();
-// //   renderer.render(<Header />);
-
-// //   expect(renderer.getRenderOutput()).toMatchSnapshot();
-
-// //   console.log(renderer.getRenderOutput());
-// // });
-
-// test('should render Header correctly', () => {
-//   const wrapper = shallow(<Header />);
-//   expect(wrapper).toMatchSnapshot();
-
-
-//   //expect(wrapper.find('h1').text()).toBe('Expensify');
-// });
-
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../componenets/Header';
+import { Header } from '../../componenets/Header';
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => { }} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
